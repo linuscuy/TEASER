5a14e5650eda8eb0f5ab506216d8f1a7a4e4e97e
DESKTOP-QJDLPMG\Linus <linus.cuypers@rwth-aachen.de>
2021-09-22 20:50:44 +0200

    Added "en15804IndicatorValue"-Class which represents values for every lifecycle stage of any environmental indicator according to EN 15804

---
+++ teaser/logic/buildingobjects/buildingphysics/en15804indicatorvalue.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+
+# created September 2021
+# by Johannes Linus Cuypers
+
+class en15804IndicatorValue(object):
+    def __init__(self, parent = None):
+        self.parent = parent
+        self._unit = None
+        
+        self._a1 = None
+        self._a2 = None
+        self._a3 = None
+        self._a1_a3 = None
+        
+        self._a4 = None
+        self._a5 = None
+        
+        self._b1 = None
+        self._b2 = None
+        self._b3 = None
+        self._b4 = None
+        self._b5 = None
+        self._b6 = None
+        self._b7 = None
+        
+        self._c1 = None
+        self._c2 = None
+        self._c3 = None
+        self._c4 = None
+        
+        self._d = None
+        
+    
+            
