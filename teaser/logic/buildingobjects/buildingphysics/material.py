825c6ccf521628c3285fa431f36a62117e61aa46
DESKTOP-QJDLPMG\Linus <linus.cuypers@rwth-aachen.de>
2021-10-30 15:14:33 +0200

    Documentation

--- teaser/logic/buildingobjects/buildingphysics/material.py
+++ teaser/logic/buildingobjects/buildingphysics/material.py
@@ -47,6 +47,10 @@ class Material(object):
     material_id : str(uuid)
         UUID of material, this is used to have similar behaviour like foreign
         key in SQL data bases for use in TypeBuildingElements and Material json
+    lca_data : En15804LcaData
+        LCA-data with indicators according to EN15804
+    service_life : int [a]
+        service life of the material
 
     """
 
