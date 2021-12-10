ab08c614c1c758650c2d14b85f7cece227bd57b3
DESKTOP-QJDLPMG\Linus <linus.cuypers@rwth-aachen.de>
2021-12-10 20:37:00 +0100

    lca-datasets are now normed

--- teaser/data/input/lca_data_input.py
+++ teaser/data/input/lca_data_input.py
@@ -39,7 +39,7 @@ def load_en15804_lca_data_id(lca_data, lca_id, data_class):
                 lca_data.lca_data_id = id
                 lca_data.name = data["name"]
 
-                lca_data.ref_flow_value = data["ref_flow"]["value"]
+                ref_flow_value = data["ref_flow"]["value"]
                 lca_data.ref_flow_unit = data["ref_flow"]["unit"]
                 
                 pere = En15804IndicatorValue()
@@ -93,30 +93,34 @@ def load_en15804_lca_data_id(lca_data, lca_id, data_class):
                 adpf.set_values(**data["adpf"]) 
                 
              
-                lca_data.pere = pere
-                lca_data.pert = pert
-                lca_data.penre = penre
-                lca_data.penrm = penrm
-                lca_data.penrt = penrt
-                lca_data.sm = sm
-                lca_data.rsf = rsf
-                lca_data.nrsf = nrsf
-                lca_data.fw = fw
-                lca_data.hwd = hwd
-                lca_data.nhwd = nhwd
-                lca_data.rwd = rwd
-                lca_data.cru = cru
-                lca_data.mfr = mfr
-                lca_data.mer = mer
-                lca_data.eee = eee
-                lca_data.eet = eet
-                lca_data.gwp = gwp
-                lca_data.odp = odp
-                lca_data.pocp = pocp
-                lca_data.ap = ap
-                lca_data.ep = ep
-                lca_data.adpe = adpe
-                lca_data.adpf = adpf
+                lca_data.pere = pere * (1/ref_flow_value)
+                lca_data.pert = pert * (1/ref_flow_value)
+                lca_data.penre = penre * (1/ref_flow_value)
+                lca_data.penrm = penrm * (1/ref_flow_value)
+                lca_data.penrt = penrt * (1/ref_flow_value)
+                lca_data.sm = sm * (1/ref_flow_value)
+                lca_data.rsf = rsf * (1/ref_flow_value)
+                lca_data.nrsf = nrsf * (1/ref_flow_value)
+                lca_data.fw = fw * (1/ref_flow_value)
+                lca_data.hwd = hwd * (1/ref_flow_value)
+                lca_data.nhwd = nhwd * (1/ref_flow_value)
+                lca_data.rwd = rwd * (1/ref_flow_value)
+                lca_data.cru = cru * (1/ref_flow_value)
+                lca_data.mfr = mfr * (1/ref_flow_value)
+                lca_data.mer = mer * (1/ref_flow_value)
+                lca_data.eee = eee * (1/ref_flow_value)
+                lca_data.eet = eet * (1/ref_flow_value)
+                lca_data.gwp = gwp * (1/ref_flow_value)
+                lca_data.odp = odp * (1/ref_flow_value)
+                lca_data.pocp = pocp * (1/ref_flow_value)
+                lca_data.ap = ap * (1/ref_flow_value)
+                lca_data.ep = ep * (1/ref_flow_value)
+                lca_data.adpe = adpe * (1/ref_flow_value)
+                lca_data.adpf = adpf * (1/ref_flow_value)
+                
+                lca_data.ref_flow_value = 1
+                
+                
                 
                 
                 if data["fallback"]:
@@ -125,6 +129,10 @@ def load_en15804_lca_data_id(lca_data, lca_id, data_class):
                     lca_data.add_fallbacks()
                 else:
                     lca_data.fallback = []
+                    
+                
+                
+                print("load:", lca_data.gwp.b6)
                         
 
 def load_en15804_lca_data_fallback_id(lca_data, lca_id, data_class):
@@ -159,7 +167,7 @@ def load_en15804_lca_data_fallback_id(lca_data, lca_id, data_class):
                 lca_data.lca_data_id = id
                 lca_data.name = data["name"]
 
-                lca_data.ref_flow_value = data["ref_flow"]["value"]
+                ref_flow_value = data["ref_flow"]["value"]
                 lca_data.ref_flow_unit = data["ref_flow"]["unit"]
                 
                 pere = En15804IndicatorValue()
@@ -212,8 +220,7 @@ def load_en15804_lca_data_fallback_id(lca_data, lca_id, data_class):
                 adpe.set_values(**data["adpe"])
                 adpf.set_values(**data["adpf"]) 
                 
-             
-                lca_data.pere = pere
+                lca_data.pere = pere 
                 lca_data.pert = pert
                 lca_data.penre = penre
                 lca_data.penrm = penrm
